66e35597be2367b22bab50797bdf71b3936c6ee7
quchengchun <quchengchun@yidian-inc.com>
2021-02-05 10:51:47 +0800

    add: 归并排序

--- 基础/归并排序.js
+++ 基础/归并排序.js
@@ -3,30 +3,31 @@ function mergeSort(arr){
     if (len < 2) {
         return arr;
     }
-    let middle = Math.floor(arr.length / 2),
+    let middle = Math.floor(len / 2),
         left = arr.slice(0, middle),
         right = arr.slice(middle);
-    return merge(mergeSort(left), mergeSort(right))
+    return merge(mergeSort(left), mergeSort(right));
 }
+
 function merge(left, right){
     let result = [];
-    while(left.length && right.length) {
+    while(left.length && right.length){
         if (left[0] <= right[0]) {
             result.push(left.shift())
         }else{
             result.push(right.shift())
         }
     }
-    while(left.length) {
+    while(left.length){
         result.push(left.shift())
     }
-    while(right.length) {
+    while(right.length){
         result.push(right.shift())
     }
     return result;
 }
 
-let arr = [88,15,66,56,1,5,6,19]
+let arr = [88,15,66,56,1,5,6,19,56]
 
 let res = mergeSort(arr);
 console.log(res)
